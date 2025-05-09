8688244fa807f63499c6a0d7104b3ccb1a12fcd7
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-09 13:28:59 +0100

    Exercicio 6 resolvido.

---
+++ FilePower2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string fileName = args[0];
+
+            Console.WriteLine("");
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input))
+                        break;
+
+                    writer.WriteLine(input);
+                }
+            }
+        }
+    }
+}
