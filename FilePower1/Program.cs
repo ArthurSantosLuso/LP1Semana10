090bad4dd01f6ab781dd2acb5f72c77fc34c86a9
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-09 13:23:38 +0100

    Exercicio 5 resolvido.

---
+++ FilePower1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string fileName = args[0];
+            Queue<string> inputQueue = new Queue<string>();
+
+            Console.WriteLine("");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                    break;
+
+                inputQueue.Enqueue(input);
+            }
+
+            File.WriteAllLines(fileName, inputQueue);
+        }
+    }
+}
--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -44,5 +44,7 @@ namespace IntCollections
                 Console.WriteLine(valor);
             }
         }
+
+                
     }
 }
